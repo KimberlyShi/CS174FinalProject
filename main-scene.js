1cfb15fd09402694ffa47d681b0491537e6fbc71
kimberlyshi <kimberly.lshi@gmail.com>
2020-02-15 20:54:22 -0800

    delete unused scenes

--- main-scene.js
+++ main-scene.js
@@ -14,36 +14,17 @@ const { Vector, Vector3, vec, vec3, vec4, color, Matrix, Mat4, Light, Shape, Mat
 const Minimal_Webgl_Demo = defs.Minimal_Webgl_Demo;
 import { Axes_Viewer, Axes_Viewer_Test_Scene } 
   from "./examples/axes-viewer.js"
-import { Inertia_Demo, Collision_Demo }
-  from "./examples/collisions-demo.js"
-import { Many_Lights_Demo }
-  from "./examples/many-lights-demo.js"
-import { Obj_File_Demo }
-  from "./examples/obj-file-demo.js"
-import { Scene_To_Texture_Demo }
-  from "./examples/scene-to-texture-demo.js"
-import { Surfaces_Demo }
-  from "./examples/surfaces-demo.js"
-import { Text_Demo }
-  from "./examples/text-demo.js"
 import { Transforms_Sandbox }
   from "./examples/transforms-sandbox.js"
 
 Object.assign( defs,
-                     { Axes_Viewer, Axes_Viewer_Test_Scene },
-                     { Inertia_Demo, Collision_Demo },
-                     { Many_Lights_Demo },
-                     { Obj_File_Demo },
-                     { Scene_To_Texture_Demo },
-                     { Surfaces_Demo },
-                     { Text_Demo },
                      { Transforms_Sandbox } );
 
     // ******************** End extra step
 
 // (Can define Main_Scene's class here)
 
-const Main_Scene = Collision_Demo;
+const Main_Scene = Transforms_Sandbox;
 const Additional_Scenes = [];
 
 export { Main_Scene, Additional_Scenes, Canvas_Widget, Code_Widget, Text_Widget, defs }
