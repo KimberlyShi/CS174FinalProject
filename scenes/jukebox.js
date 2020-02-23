61e7bc0529de684b46440eb308dc3270aec8bbb7
Dade Huang <dadehuang@gmail.com>
2020-02-23 09:13:13 -0800

    update

--- scenes/jukebox.js
+++ scenes/jukebox.js
@@ -16,70 +16,8 @@ export class Jukebox_Base extends Scene
       super();
 
       // Load the model file:
-      this.shapes = { "jukebox": new Shape_From_File( "assets/jukebox.obj" ) };
+      //this.shapes = { "jukebox": new Shape_From_File( "assets/jukebox.obj" ) };
 
-
-      // this.hover = this.swarm = false;
-                                                        // // At the beginning of our program, load one of each of these shape 
-                                                        // // definitions onto the GPU.  NOTE:  Only do this ONCE per shape it
-                                                        // // would be redundant to tell it again.  You should just re-use the
-                                                        // // one called "box" more than once in display() to draw multiple cubes.
-                                                        // // Don't define more than one blueprint for the same thing here.
-      // this.shapes = { 'box'  : new Cube(),
-                      // 'ball' : new Subdivision_Sphere( 4 ) };
-      
-                                                  // // *** Materials: *** Define a shader, and then define materials that use
-                                                  // // that shader.  Materials wrap a dictionary of "options" for the shader.
-                                                  // // Here we use a Phong shader and the Material stores the scalar 
-                                                  // // coefficients that appear in the Phong lighting formulas so that the
-                                                  // // appearance of particular materials can be tweaked via these numbers.
-      // const phong = new defs.Phong_Shader();
-      // this.materials = { plastic: new Material( phong,
-                                    // { ambient: .2, diffusivity: 1, specularity: .5, color: color( .9,.5,.9,1 ) } ),
-                           // metal: new Material( phong, 
-                                    // { ambient: .2, diffusivity: 1, specularity:  1, color: color( .9,.5,.9,1 ) } ) };
-    }
-  make_control_panel()
-    {                                 // make_control_panel(): Sets up a panel of interactive HTML elements, including
-                                      // buttons with key bindings for affecting this scene, and live info readouts.
-      this.control_panel.innerHTML += "Dragonfly rotation angle: <br>";
-                                                // The next line adds a live text readout of a data member of our Scene.
-      this.live_string( box => { box.textContent = ( this.hover ? 0 : ( this.t % (2*Math.PI)).toFixed(2) ) + " radians" } ); 
-      this.new_line();
-                                                // Add buttons so the user can actively toggle data members of our Scene:
-      this.key_triggered_button( "Hover dragonfly in place", [ "h" ], function() { this.hover ^= 1; } );
-      this.new_line();
-      this.key_triggered_button( "Swarm mode", [ "m" ], function() { this.swarm ^= 1; } );
-    }
-  display( context, program_state )
-    {                                                // display():  Called once per frame of animation.  We'll isolate out
-                                                     // the code that actually draws things into Transforms_Sandbox, a
-                                                     // subclass of this Scene.  Here, the base class's display only does
-                                                     // some initial setup.
-     
-                           // Setup -- This part sets up the scene's overall camera matrix, projection matrix, and lights:
-/*						   
-      if( !context.scratchpad.controls ) 
-        { this.children.push( context.scratchpad.controls = new defs.Movement_Controls() ); 
-
-                    // Define the global camera and projection matrices, which are stored in program_state.  The camera
-                    // matrix follows the usual format for transforms, but with opposite values (cameras exist as 
-                    // inverted matrices).  The projection matrix follows an unusual format and determines how depth is 
-                    // treated when projecting 3D points onto a plane.  The Mat4 functions perspective() and
-                    // orthographic() automatically generate valid matrices for one.  The input arguments of
-                    // perspective() are field of view, aspect ratio, and distances to the near plane and far plane.
-          program_state.set_camera( Mat4.translation( 0,3,-10 ) );
-        }
-      program_state.projection_transform = Mat4.perspective( Math.PI/4, context.width/context.height, 1, 100 );
-      
-                                                // *** Lights: *** Values of vector or point lights.  They'll be consulted by 
-                                                // the shader when coloring shapes.  See Light's class definition for inputs.
-      const t = this.t = program_state.animation_time/1000;
-      const angle = Math.sin( t );
-      const light_position = Mat4.rotation( angle,   1,0,0 ).times( vec4( 0,-1,1,0 ) );
-      program_state.lights = [ new Light( light_position, color( 1,1,1,1 ), 1000000 ) ];
-
-      this.shapes.jukebox.draw( context, program_state, model_transform); */
     }
 }
 
@@ -92,8 +30,8 @@ export class Play_Jukebox extends Jukebox_Base
                                                      // the shapes.  We isolate that code so it can be experimented with on its own.
                                                      // This gives you a very small code sandbox for editing a simple scene, and for
                                                      // experimenting with matrix transformations.
-  display( context, program_state )
-    {                                                // display():  Called once per frame of animation.  For each shape that you want to
+  /*display( context, program_state )
+    {                                                 // display():  Called once per frame of animation.  For each shape that you want to
                                                      // appear onscreen, place a .draw() call for it inside.  Each time, pass in a
                                                      // different matrix value to control where the shape appears.
 
@@ -110,9 +48,9 @@ export class Play_Jukebox extends Jukebox_Base
                                                 // Call the setup code that we left inside the base class:
       super.display( context, program_state );
 
-      /**********************************
-      Start coding down here!!!!
-      **********************************/         
+
+      //Start coding down here!!!!
+      
                                                   // From here on down it's just some example shapes drawn for you -- freely 
                                                   // replace them with your own!  Notice the usage of the Mat4 functions 
                                                   // translation(), scale(), and rotation() to generate matrices, and the 
@@ -179,5 +117,6 @@ export class Play_Jukebox extends Jukebox_Base
                               // scale() last and then immediately unscale after the draw.  Or better yet, don't store
                               // the scaled matrix back in model_transform at all -- but instead in just a temporary
                               // expression that we pass into draw(), or store under a different name.
-    }
+							 
+    } */
 }
