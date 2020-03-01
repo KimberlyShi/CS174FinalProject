16592f5ab6f02ad873895f425f1fad7a59ebcece
kimberlyshi <kimberly.lshi@gmail.com>
2020-03-01 15:14:37 -0800

    update camera movement control panel

--- scenes/common.js
+++ scenes/common.js
@@ -794,47 +794,47 @@ class Movement_Controls extends Scene
       this.key_triggered_button( "+",  [ "p" ], () =>
                                             this.speed_multiplier  *=  1.2, "green", undefined, undefined, speed_controls );
       this.new_line();
-      this.key_triggered_button( "Roll left",  [ "," ], () => this.roll =  1, undefined, () => this.roll = 0 );
-      this.key_triggered_button( "Roll right", [ "." ], () => this.roll = -1, undefined, () => this.roll = 0 );
+      this.key_triggered_button( "Rotate left",  [ "," ], () => this.roll =  1, undefined, () => this.roll = 0 );
+      this.key_triggered_button( "Rotate right", [ "." ], () => this.roll = -1, undefined, () => this.roll = 0 );
 
       // this.key_triggered_button( "Roll left",  [ "," ], () => this.pan =  1, undefined, () => this.pan = 0 );
       // this.key_triggered_button( "Roll right", [ "." ], () => this.pan = -1, undefined, () => this.pan = 0 );
 
-      this.new_line();
-      this.key_triggered_button( "(Un)freeze mouse look around", [ "f" ], () => this.look_around_locked ^=  1, "green" );
-      this.new_line();
-      this.live_string( box => box.textContent = "Position: " + this.pos[0].toFixed(2) + ", " + this.pos[1].toFixed(2)
-                                                       + ", " + this.pos[2].toFixed(2) );
-      this.new_line();
+      // this.new_line();
+      // this.key_triggered_button( "(Un)freeze mouse look around", [ "f" ], () => this.look_around_locked ^=  1, "green" );
+      // this.new_line();
+      // this.live_string( box => box.textContent = "Position: " + this.pos[0].toFixed(2) + ", " + this.pos[1].toFixed(2)
+      //                                                  + ", " + this.pos[2].toFixed(2) );
+      // this.new_line();
                                                   // The facing directions are surprisingly affected by the left hand rule:
-      this.live_string( box => box.textContent = "Facing: " + ( ( this.z_axis[0] > 0 ? "West " : "East ")
-                   + ( this.z_axis[1] > 0 ? "Down " : "Up " ) + ( this.z_axis[2] > 0 ? "North" : "South" ) ) );
-      this.new_line();
-      this.key_triggered_button( "Go to world origin", [ "r" ], () => { this. matrix().set_identity( 4,4 );
-                                                                        this.inverse().set_identity( 4,4 ) }, "orange" );
-      this.new_line();
+      // this.live_string( box => box.textContent = "Facing: " + ( ( this.z_axis[0] > 0 ? "West " : "East ")
+      //              + ( this.z_axis[1] > 0 ? "Down " : "Up " ) + ( this.z_axis[2] > 0 ? "North" : "South" ) ) );
+      // this.new_line();
+      // this.key_triggered_button( "Go to world origin", [ "r" ], () => { this. matrix().set_identity( 4,4 );
+      //                                                                   this.inverse().set_identity( 4,4 ) }, "orange" );
+      // this.new_line();
 
-      this.key_triggered_button( "Look at origin from front", [ "1" ], () =>
-        { this.inverse().set( Mat4.look_at( vec3( 0,0,10 ), vec3( 0,0,0 ), vec3( 0,1,0 ) ) );
-          this. matrix().set( Mat4.inverse( this.inverse() ) );
-        }, "black" );
-      this.new_line();
-      this.key_triggered_button( "from right", [ "2" ], () =>
-        { this.inverse().set( Mat4.look_at( vec3( 10,0,0 ), vec3( 0,0,0 ), vec3( 0,1,0 ) ) );
-          this. matrix().set( Mat4.inverse( this.inverse() ) );
-        }, "black" );
-      this.key_triggered_button( "from rear", [ "3" ], () =>
-        { this.inverse().set( Mat4.look_at( vec3( 0,0,-10 ), vec3( 0,0,0 ), vec3( 0,1,0 ) ) );
-          this. matrix().set( Mat4.inverse( this.inverse() ) );
-        }, "black" );
-      this.key_triggered_button( "from left", [ "4" ], () =>
-        { this.inverse().set( Mat4.look_at( vec3( -10,0,0 ), vec3( 0,0,0 ), vec3( 0,1,0 ) ) );
-          this. matrix().set( Mat4.inverse( this.inverse() ) );
-        }, "black" );
-      this.new_line();
-      this.key_triggered_button( "Attach to global camera", [ "Shift", "R" ],
-                                                 () => { this.will_take_over_graphics_state = true }, "blue" );
-      this.new_line();
+      // this.key_triggered_button( "Look at origin from front", [ "1" ], () =>
+      //   { this.inverse().set( Mat4.look_at( vec3( 0,0,10 ), vec3( 0,0,0 ), vec3( 0,1,0 ) ) );
+      //     this. matrix().set( Mat4.inverse( this.inverse() ) );
+      //   }, "black" );
+      // this.new_line();
+      // this.key_triggered_button( "from right", [ "2" ], () =>
+      //   { this.inverse().set( Mat4.look_at( vec3( 10,0,0 ), vec3( 0,0,0 ), vec3( 0,1,0 ) ) );
+      //     this. matrix().set( Mat4.inverse( this.inverse() ) );
+      //   }, "black" );
+      // this.key_triggered_button( "from rear", [ "3" ], () =>
+      //   { this.inverse().set( Mat4.look_at( vec3( 0,0,-10 ), vec3( 0,0,0 ), vec3( 0,1,0 ) ) );
+      //     this. matrix().set( Mat4.inverse( this.inverse() ) );
+      //   }, "black" );
+      // this.key_triggered_button( "from left", [ "4" ], () =>
+      //   { this.inverse().set( Mat4.look_at( vec3( -10,0,0 ), vec3( 0,0,0 ), vec3( 0,1,0 ) ) );
+      //     this. matrix().set( Mat4.inverse( this.inverse() ) );
+      //   }, "black" );
+      // this.new_line();
+      // this.key_triggered_button( "Attach to global camera", [ "Shift", "R" ],
+      //                                            () => { this.will_take_over_graphics_state = true }, "blue" );
+      // this.new_line();
     }
   first_person_flyaround( radians_per_frame, meters_per_frame, leeway = 70 )
     {                                                     // (Internal helper function)
